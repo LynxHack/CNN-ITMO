b29f9722e3b18f24618671395dadd371f0edbf88
LynxHack <KevinLynx_L@hotmail.com>
2019-04-11 16:35:14 -0700

    Added changes to network layer size to match shapes

--- model.py
+++ model.py
@@ -264,22 +264,22 @@ def U_net(pretrained_weights = None, input_size = (512,512,1)):
     ##transposed conv
     ##upsample fiter size 4 (2*2),strides (2) 
     ##concatenate on axis 3
-    up6 = ConvBNTranspose(256, 4, drop_cross)##(UpSampling2D(size = (2,2))(conv_cross))
+    up6 = ConvBNTranspose(256, 2, drop_cross)##(UpSampling2D(size = (2,2))(conv_cross))
     merge6 = concatenate([drop4, up6], axis = 3)
     conv6 = ConvBN(256, 3, merge6)
     conv6 = ConvBN(256, 3, conv6)
                                                                                        
-    up7 = ConvBNTranspose(128, 4, conv6)##(UpSampling2D(size = (2,2))(conv6))
+    up7 = ConvBNTranspose(128, 2, conv6)##(UpSampling2D(size = (2,2))(conv6))
     merge7 = concatenate([conv3,up7], axis = 3)
     conv7 = ConvBN(128, 3,merge7)
     conv7 = ConvBN(128, 3,conv7)
 
-    up8 = ConvBNTranspose(64, 4, conv7)##(UpSampling2D(size = (2,2))(conv7))
+    up8 = ConvBNTranspose(64, 2, conv7)##(UpSampling2D(size = (2,2))(conv7))
     merge8 = concatenate([conv2,up8], axis = 3)
     conv8 = ConvBN(64, 3, merge8)
     conv8 = ConvBN(64, 3, conv8)
 
-    up9 = ConvBNTranspose(32, 4, conv8)##(UpSampling2D(size = (2,2))(conv8))
+    up9 = ConvBNTranspose(32, 2, conv8)##(UpSampling2D(size = (2,2))(conv8))
     merge9 = concatenate([conv1,up9], axis = 3)
     conv9 = ConvBN(32, 3, merge9)
     conv9 = ConvBN(32, 3, conv9)
@@ -295,7 +295,7 @@ def U_net(pretrained_weights = None, input_size = (512,512,1)):
     ## Use Sigmoid here (changed by zz)      
     ## One dimension in Z axis, and 3*3 filter size
     ## Second parameter 1 or 3
-    OutImage = Conv2D(1, 3, activation = 'sigmoid')(conv9)
+    OutImage = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(input = inputs, output = OutImage, name='Reinhardt Prediction')
     # Adam Optimizer
