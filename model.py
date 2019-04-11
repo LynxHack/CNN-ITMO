e36be68dd278ebeec474db93aa069563ab2a2ee9
Chisiu <35022169+zixiaoubc@users.noreply.github.com>
2019-04-10 18:43:53 -0700

    Update model.py

--- model.py
+++ model.py
@@ -264,28 +264,28 @@ def U_net(pretrained_weights = None, input_size = (512,512,1)):
     ##transposed conv
     ##upsample fiter size 4 (2*2),strides (2) 
     ##concatenate on axis 3
-    up6 = ConvBNTranspose(512, 2, drop_cross)##(UpSampling2D(size = (2,2))(conv_cross))
+    up6 = ConvBNTranspose(512, 4, drop_cross)##(UpSampling2D(size = (2,2))(conv_cross))
     merge6 = concatenate([drop5, up6], axis = 3)
     conv6 = ConvBN(512, 3, merge6)
     conv6 = ConvBN(512, 3, conv6)
                                                                                        
-    up7 = ConvBNTranspose(256, 2, conv6)##(UpSampling2D(size = (2,2))(conv6))
+    up7 = ConvBNTranspose(256, 4, conv6)##(UpSampling2D(size = (2,2))(conv6))
     merge7 = concatenate([drop4,up7], axis = 3)
     conv7 = ConvBN(256, 3,merge7)
     conv7 = ConvBN(256, 3,conv7)
 
-    up8 = ConvBNTranspose(128, 2, conv7)##(UpSampling2D(size = (2,2))(conv7))
+    up8 = ConvBNTranspose(128, 4, conv7)##(UpSampling2D(size = (2,2))(conv7))
     merge8 = concatenate([conv3,up8], axis = 3)
     conv8 = ConvBN(128, 3, merge8)
     conv8 = ConvBN(128, 3, conv8)
 
-    up9 = ConvBNTranspose(64, 2, conv8)##(UpSampling2D(size = (2,2))(conv8))
+    up9 = ConvBNTranspose(64, 4, conv8)##(UpSampling2D(size = (2,2))(conv8))
     merge9 = concatenate([conv2,up9], axis = 3)
     conv9 = ConvBN(64, 3, merge9)
     conv9 = ConvBN(64, 3, conv9)
     # conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     
-    up10 = ConvBNTranspose(32, 2, conv9)##(UpSampling2D(size = (2,2))(conv9))
+    up10 = ConvBNTranspose(32, 4, conv9)##(UpSampling2D(size = (2,2))(conv9))
     merge10 = concatenate([conv1,up10], axis = 3)
     conv10 = ConvBN(32, 3, merge10)
     conv10 = ConvBN(32, 3, conv10)
